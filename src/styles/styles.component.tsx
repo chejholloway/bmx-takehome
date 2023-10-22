efaeb23bd2c2c2bf2f8dd4742b3d4dc93290986c
Che' J. Holloway <chejholloway@outlook.com>
2023-10-22 16:41:16 -0700

    refactor(global): :construction: fixing minor issues

--- src/styles/styles.component.tsx
+++ src/styles/styles.component.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import tw from "twin.macro";
 
-export const BrightApp = tw.div`container h-screen w-screen top-0 left-0 fixed`;
+export const BrightApp = tw.div`container max-h-screen min-w-min top-0 left-0 fixed`;
 export const Header = tw.header`p-0 m-0 border-0`;
 export const MainContent = tw.main`p-0 m-0`;
 
@@ -24,7 +24,7 @@ export const FooterContainer = tw.footer`bg-blue-dark pb-8 pt-2 row text-center`
 export const FooterText = tw.p`text-white font-sans`;
 
 export const FSRow = tw.section`row p-4`;
-export const LeftColumn = tw.div`col-6 font-bold text-gray-400`;
+export const LeftColumn = tw.div`col-6 font-medium text-gray-500 text-sm`;
 export const RightColumn = tw.div`col-6 relative left-[8rem]`;
 export const Figure = tw.figure``;
 export const Image = tw.img``;
@@ -33,7 +33,7 @@ export const LogoRowStyles = tw.section`row bg-blue pt-4 border-0`;
 export const LogoColumn = tw.div`col-10`;
 export const IconColumn = tw.div`col-1 m-0 p-0 relative top-1`;
 
-export const NavMenuStyles = tw.nav`border-0`
+export const NavMenuStyles = tw.nav`border-0`;
 export const NavList = tw.ul`bg-blue row py-8`;
 export const NavLink = tw.li`col p-0 m-0 inline text-xs font-sans text-white relative left-[12px]`;
 export const CurrentNavLink = tw.li`col relative bottom-1 py-1 px-3 inline text-xs font-sans text-white text-center rounded-2xl bg-blue-dark`;
@@ -46,13 +46,13 @@ export const Update = tw.p`relative right-12 text-xs text-slate-500 font-br`;
 export const SunCircleColumn = tw.div`col-3`;
 export const TextColumn = tw.div`col-9`;
 
-export const Month = tw.span`mt-8 text-xs font-br text-gray-500 mt-8 text-xs font-br text-gray-500`;
+export const Month = tw.span`mt-8 text-xs font-br text-gray-500`;
 export const TogglesSection = tw.div`row text-center pb-4 -ml-12`;
 export const SeptLabel = tw.label`relative inline-flex cursor-pointer`;
 export const SeptColumn = tw.div`col-6`;
 export const SeptToggle =
   "-ml-16 w-11 h-4 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:bg-blue-dark peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[-7px] after:left-6 after:bg-gray-400 after:border-gray-300 after:border after:rounded-full after:h-7 after:w-7 after:transition-all border-gray-600";
-export const AugLabel = tw.label`relative inline-flex cursor-pointer`;
+export const AugLabel = tw.label`relative inline-flex cursor-pointer text-gray-500`;
 export const AugColumn = tw.div`col-6`;
 export const AugToggle =
   "relative right-[3.5rem] w-11 h-4 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:bg-blue-dark peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[-6px] after:left-0 after:bg-gray-400 after:border-gray-300 after:border after:rounded-full after:h-7 after:w-7 after:transition-all border-gray-600";
